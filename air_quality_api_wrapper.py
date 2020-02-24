dbf5030a66ccf8fdcc6cd87003c4cbaf291780c0
Anton Petrosyuk <24464919+Petrosyuk@users.noreply.github.com>
2020-02-24 00:15:08 -0500

    Added AirQualityClient class

--- air_quality_api_wrapper.py
+++ air_quality_api_wrapper.py
@@ -10,22 +10,35 @@ import requests as re
 API_BASE_URL = 'https://api.waqi.info/feed'
 API_TOKEN = os.getenv('AIR_QUALITY_OPEN_DATA_PLATFORM_TOKEN')
 
+# lat_lang = (42.648376, -73.707518)
 
-def get_loc_results():
-    lat_lang = (42.648376, -73.707518)
+class AirQualityClient(object):
+    def __init__(self):
+        pass
+    
+    def get_by_loc(self, lat:float,lng:float):
+        # Returns api data based on geo cordinates.
+        rest_path = f'{API_BASE_URL}/geo:{lat};{lng}/?token={API_TOKEN}'
+        rsp = re.get(rest_path)
+        self._printer(rsp)
 
-def get_city_feed():
-    # Returns a city feed request status
-    city = 'new york city'
+    def get_by_city(self,city:str):
+        # Returns data by city name
+        rest_path = f'{API_BASE_URL}/{city}/?token={API_TOKEN}'
+        rsp = re.get(rest_path)
+        self._printer(rsp)
 
-    rest_path = f'{API_BASE_URL}/{city}/?token={API_TOKEN}'
-    # rest_path = 'https://api.waqi.info/feed/shanghai/?token=demo'
-    print(rest_path)
-    rsp = re.get(rest_path)
-
-    print(rsp.status_code)
-    print(rsp.json())
+    def _printer(self,rsp):
+        # Returns filtered JSON data from the API response
+        assert (rsp is not None), 'Response did not return an object.'
+        # if rsp.status_code != 200:
+        #     raise 
+        print(rsp.status_code)
+        json_object = rsp.json()
+        print(json_object)
+        return json_object
 
 
 if __name__ == "__main__":
-    get_city_feed()
+    aq = AirQualityClient()
+    aq.get_by_loc(42.648376, -73.707518)
